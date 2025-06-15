d7f82f98e5137e55fd556bc4f40561654fb40cbc
younes el mezouari <109693920+elmzouari@users.noreply.github.com>
2025-06-15 15:58:33 +0100

    Add files via upload

---
+++ alx-project-0x04/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
